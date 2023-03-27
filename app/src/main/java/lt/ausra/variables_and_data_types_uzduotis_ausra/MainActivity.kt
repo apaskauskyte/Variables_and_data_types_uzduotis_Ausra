5677522d2d7eaec3b1060b329cf5675e3e355bcc
apaskauskyte <a.paskauskyte@gmail.com>
2023-03-27 16:41:11 +0300

    Add variables maxByte, maxShort, maxInt, maxLong

--- app/src/main/java/lt/ausra/variables_and_data_types_uzduotis_ausra/MainActivity.kt
+++ app/src/main/java/lt/ausra/variables_and_data_types_uzduotis_ausra/MainActivity.kt
@@ -2,10 +2,24 @@ package lt.ausra.variables_and_data_types_uzduotis_ausra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "kotlin_variables_tag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doTaskVariablesAndDataTypes()
+    }
+
+    fun doTaskVariablesAndDataTypes() {
+
+        val maxByte = Byte.MAX_VALUE
+        val maxShort = Short.MAX_VALUE
+        val maxInt = Int.MAX_VALUE
+        val maxLong = Long.MAX_VALUE
     }
 }
